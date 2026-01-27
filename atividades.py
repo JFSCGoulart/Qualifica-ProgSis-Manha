a05fb8a2b448c8a274917c06ebfd1e68d35b79c6
just1kiki <kikialg2003@gmail.com>
2026-01-27 11:51:25 -0300

    Criação do arquivo Atividades

---
+++ atividades.py
@@ -0,0 +1,18 @@
+# Fazer atividades variadas:
+
+
+#o Múltipla escolha
+#o Verdadeiro ou Falso
+#o Preencher lacunas
+#o Ordenar etapas
+#o Sequências lógicas
+#o Correspondência (ligar colunas)
+#o Classificação (separar categorias)
+#o Escolha múltipla (várias corretas)
+#o Palavra embaralhada
+#o Mini-cenários com decisões
+
+
+
+#• Pedir dica antes de responder
+#• Ganhar 1 estrela por acerto (sem penalidade por erro)
