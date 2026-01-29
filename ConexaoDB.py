177ed00063b9bd48f5d588178ff0ee854d2c0802
leticiamonteiro15 <164946563+leticiamonteiro15@users.noreply.github.com>
2026-01-29 09:27:25 -0300

    LETICIA- add banco

--- ConexaoDB.py
+++ ConexaoDB.py
@@ -1,47 +1,49 @@
-import _sqlite3 
+import sqlite3 
 
-conexao= _sqlite3.connect("sistema_db")
+conexao= sqlite3.connect('sistema.db')
 cursor= conexao.cursor()
 
-cursor.execute('''
-	CREATE TABLE IF NOT EXISTS cadastro_usuario (
-		id INTEGER NOT NULL UNIQUE,
-		nome_completo TEXT,
-		data_nascimento DATE,
-		CPF INTEGER,
-		CEP INTEGER,
-		Endereco TEXT,
-		telefone INTEGER,
-		email TEXT,
-		tipo_user VARCHAR,
-		senha_hash TEXT,
-		PRIMARY KEY("id")
-	);
+cursor.executescript('''
+CREATE TABLE IF NOT EXISTS "cadastro_usuario" (
+	"id" INTEGER NOT NULL UNIQUE,
+	"nome_completo" TEXT,
+	"data_nascimento" DATE,
+	"CPF" INTEGER,
+	"CEP" INTEGER,
+	"Endereco" TEXT,
+	"telefone" INTEGER,
+	"email" TEXT,
+	"tipo_user" VARCHAR,
+	"senha_hash" TEXT,
+	PRIMARY KEY("id")
+);
 
-	CREATE TABLE IF NOT EXISTS cursos (
-		id INTEGER NOT NULL UNIQUE,
-		nome_completo TEXT NOT NULL,
-		PRIMARY KEY("id", "nome_completo")
-	);
+CREATE TABLE IF NOT EXISTS "Cursos" (
+	"id" INTEGER NOT NULL UNIQUE,
+	"nome_completo" TEXT NOT NULL,
+	PRIMARY KEY("id", "nome_completo")
+);
 
-	CREATE TABLE IF NOT EXISTS atividades (
-		id INTEGER NOT NULL UNIQUE,
-		curso_id INTEGER,
-		tipo TEXT,
-		perguntas TEXT,
-		opcoes TEXT,
-		resposta_correta TEXT,
-		dica TEXT,
-		pontuacoes INTEGER,
-		PRIMARY KEY("id")
-	);
+CREATE TABLE IF NOT EXISTS "atividades" (
+	"id" INTEGER NOT NULL UNIQUE,
+	"curso_id" INTEGER,
+	"tipo" TEXT,
+	"perguntas" TEXT,
+	"opcoes" TEXT,
+	"resposta_correta" TEXT,
+	"dica" TEXT,
+	"pontuacoes" INTEGER,
+	PRIMARY KEY("id")
+);
 
-	CREATE TABLE IF NOT EXISTS ranking_pontuacoes (
-		id INTEGER NOT NULL UNIQUE,
-		CPF INTEGER,
-		pontuacao_id INTEGER NOT NULL,
-		PRIMARY KEY("id", "pontuacao_id")
-	);
+CREATE TABLE IF NOT EXISTS "ranking_pontuacoes" (
+	"id" INTEGER NOT NULL UNIQUE,
+	"CPF" INTEGER,
+	"pontuacao_id" INTEGER NOT NULL,
+	PRIMARY KEY("id", "pontuacao_id")
+);
+
  ''')
 
 conexao.commit()
+
