6361d267ab3d325d33f38211a87a50c3e4be75fd
leticiamonteiro15 <164946563+leticiamonteiro15@users.noreply.github.com>
2026-01-29 09:59:43 -0300

    ALTEREI MAIN.PY

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
 #1 cadastro
 
-print("Cadastre-se") #Front-end(criar um botão)
-nome= input("Nome Completo: ") #armazenar no banco de dados
-data_nascimento= input("Data de Nascimento: ") 
-CPF= int(input("CPF: "))
-CEP= int(input("CEP: "))
-endereco= input("Endereço: ")
-telefone= int(input("Telefone: "))
-email= input("E-mail: ")
-tipo_user= input("Você é ALUNO, PROFESSOR ou COORDENADOR?").upper()
-senha= input("Escolha uma senha: ")
-#armazenar em TABELA  CADASTRO
-#puxar nome do usario
-
-print(f"Seja bem-vindo(a) {nome} a plataforma")#informativo do front end
+def cadastrar_usuario():
+  print("Cadastre-se") #Front-end(criar um botão)
+  nome= input("Nome Completo: ") #armazenar no banco de dados
+  data_nascimento= input("Data de Nascimento: ") #armazenar no banco de dados
+  CPF= int(input("CPF: ")) #armazenar no banco de dados
+  telefone= int(input("Telefone: ")) #armazenar no banco de dados
+  id_tipo_usuario= int(input("Escolha uma opção: Aluno(1), Professor(2), Coordenador(3) ")) #armazenar no banco de dados / front end (fazer botão de seleção)
+  senha= input("Escolha uma senha: ") #armazenar no banco de dados
+  print(f"Seja bem-vindo(a) {nome} a plataforma") #informativo do front end
