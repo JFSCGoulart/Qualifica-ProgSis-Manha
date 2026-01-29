b28c71f333801f77aa48599ccbba6502f5069d13
Izadora-Maria <izadoracursos04@gmail.com>
2026-01-29 10:46:57 -0300

    banco de dados 1

--- ConexaoDB.py
+++ ConexaoDB.py
@@ -4,46 +4,50 @@ conexao= sqlite3.connect('sistema.db')
 cursor= conexao.cursor()
 
 cursor.executescript('''
-CREATE TABLE IF NOT EXISTS "cadastro_usuario" (
-	"id" INTEGER NOT NULL UNIQUE,
-	"nome_completo" TEXT,
-	"data_nascimento" DATE,
-	"CPF" INTEGER,
-	"CEP" INTEGER,
-	"Endereco" TEXT,
-	"telefone" INTEGER,
-	"email" TEXT,
-	"tipo_user" VARCHAR,
-	"senha_hash" TEXT,
-	PRIMARY KEY("id")
-);
+	CREATE TABLE IF NOT EXISTS usuario  (
+		id INTEGER NOT NULL UNIQUE,
+		nome_completo TEXT NOT NULL,
+		data_nascimento DATE NOT NULL,
+		CPF INTEGER NOT NULL,
+		telefone INTEGER NOT NULL,
+		id_tipo_usuario VARCHAR NOT NULL,
+		senha TEXT NOT NULL,
+		PRIMARY KEY(id)
+	);
 
-CREATE TABLE IF NOT EXISTS "Cursos" (
-	"id" INTEGER NOT NULL UNIQUE,
-	"nome_completo" TEXT NOT NULL,
-	PRIMARY KEY("id", "nome_completo")
-);
+	CREATE TABLE IF NOT EXISTS tipo_usuario (
+		id INTEGER NOT NULL UNIQUE,
+		aluno,
+		professor,
+		coordenador,
+		PRIMARY KEY(id)
+	);
 
-CREATE TABLE IF NOT EXISTS "atividades" (
-	"id" INTEGER NOT NULL UNIQUE,
-	"curso_id" INTEGER,
-	"tipo" TEXT,
-	"perguntas" TEXT,
-	"opcoes" TEXT,
-	"resposta_correta" TEXT,
-	"dica" TEXT,
-	"pontuacoes" INTEGER,
-	PRIMARY KEY("id")
-);
+	CREATE TABLE IF NOT EXISTS cursos (
+		id INTEGER NOT NULL UNIQUE,
+		nome TEXT NOT NULL,
+		PRIMARY KEY(id)
+	);
 
-CREATE TABLE IF NOT EXISTS "ranking_pontuacoes" (
-	"id" INTEGER NOT NULL UNIQUE,
-	"CPF" INTEGER,
-	"pontuacao_id" INTEGER NOT NULL,
-	PRIMARY KEY("id", "pontuacao_id")
-);
+	CREATE TABLE IF NOT EXISTS atividades (
+		id INTEGER NOT NULL UNIQUE,
+		cursos_id INTEGER NOT NULL,
+		tipo TEXT NOT NULL,
+		perguntas TEXT NOT NULL,
+		opcoes TEXT NOT NULL,
+		respostas TEXT NOT NULL,
+		dica TEXT NOT NULL,
+		pontuacoes INTEGER NOT NULL,
+		PRIMARY KEY(id)
+	);
 
- ''')
+	CREATE TABLE IF NOT EXISTS ranking_pontuacoes (
+		id INTEGER NOT NULL UNIQUE,
+		CPF INTEGER NOT NULL,
+		pontuacao_id INTEGER NOT NULL,
+		PRIMARY KEY(id)
+	);
+	''')
 
 conexao.commit()
 
