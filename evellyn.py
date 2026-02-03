a1ed57492b2dc557da73350c0ede2e90ab93ef1d
EvellynAF <evellynstu@gmail.com>
2026-02-03 09:47:08 -0300

    Update evellyn.py

--- evellyn.py
+++ evellyn.py
@@ -4,44 +4,27 @@
 #• Cada tipo tem acesso a um menu diferente.
 def login_usuario():
     pass
-
-import usuarios
-import atividades
-import progresso
+def cadastrar_usuario():
+    pass
 
 while True:
-    print("Bem-vindo(a) ao Progrma do QualificaDF!")
+    print("Bem-vindo(a) ao Programa do QualificaDF!")
     print("[ 1 ] --> Aluno(a)") # Botão
     print("[ 2 ] --> Professor(a)") # Botão
     print("[ 3 ] --> Coordenador(a)") # Botão
     selecao_tipo=int(input("Selecione uma opção: "))
     match selecao_tipo:
         case 1:
-            print("Selecione o tipo de usuário:")
-            print("[ 1 ] --> Usuário Existente") # Botão
-            print("[ 2 ] --> Usuário Novo") # Botão
-            selecao_usuario=int(input("Selecione uma opção: "))
-            if selecao_usuario==1:
-                pass
-            elif selecao_usuario==2:
+            login_usuario()
+            if ValueError:
                 cadastrar_usuario() # Parte que Luiz fez/tá fazendo
         case 2:
-            print("Selecione o tipo de usuário:")
-            print("[ 1 ] --> Usuário Existente") # Botão
-            print("[ 2 ] --> Usuário Novo") # Botão
-            selecao_usuario=int(input("Selecione uma opção: "))
-            if selecao_usuario==1:
-                pass
-            elif selecao_usuario==2:
+            login_usuario()
+            if ValueError:
                 cadastrar_usuario() # Parte que Luiz fez/tá fazendo
         case 3:
-            print("Selecione o tipo de usuário:")
-            print("[ 1 ] --> Usuário Existente") # Botão
-            print("[ 2 ] --> Usuário Novo") # Botão
-            selecao_usuario=int(input("Selecione uma opção: "))
-            if selecao_usuario==1:
-                pass
-            elif selecao_usuario==2:
+            login_usuario()
+            if ValueError:
                 cadastrar_usuario() # Parte que Luiz fez/tá fazendo
         case _:
             print("Opção inválida. Tente novamente.")
