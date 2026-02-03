2a5f74e04b5e2f1917e945ace3b8d751743007aa
EvellynAF <evellynstu@gmail.com>
2026-02-03 11:47:52 -0300

    Update evellyn.py

--- evellyn.py
+++ evellyn.py
@@ -2,29 +2,7 @@
 #• Depois digita nome e senha.
 #• Se for um nome novo, o sistema pergunta o tipo e cadastra. ✅
 #• Cada tipo tem acesso a um menu diferente.
-def login_usuario():
-    pass
-def cadastrar_usuario():
-    pass
+import sqlite3 
 
-while True:
-    print("Bem-vindo(a) ao Programa do QualificaDF!")
-    print("[ 1 ] --> Aluno(a)") # Botão
-    print("[ 2 ] --> Professor(a)") # Botão
-    print("[ 3 ] --> Coordenador(a)") # Botão
-    selecao_tipo=int(input("Selecione uma opção: "))
-    match selecao_tipo:
-        case 1:
-            login_usuario()
-            if ValueError:
-                cadastrar_usuario() # Parte que Luiz fez/tá fazendo
-        case 2:
-            login_usuario()
-            if ValueError:
-                cadastrar_usuario() # Parte que Luiz fez/tá fazendo
-        case 3:
-            login_usuario()
-            if ValueError:
-                cadastrar_usuario() # Parte que Luiz fez/tá fazendo
-        case _:
-            print("Opção inválida. Tente novamente.")
+conexao= sqlite3.connect('sistema.db')
+cursor= conexao.cursor()
