f391c5e7a4a113a7d405237cdc4fa8ac76ffab8f
EvellynAF <evellynstu@gmail.com>
2026-02-03 09:30:26 -0300

    Update evellyn.py

--- evellyn.py
+++ evellyn.py
@@ -4,12 +4,18 @@ cursor=conexao.cursor()
 # criar tabela
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS opcoes(
+<<<<<<< Updated upstream
         id_opcoes INTEGER PRIMARY KEY UNIQUE,
+=======
+        id INTEGER PRIMARY KEY UNIQUE,
+        id_atividade INT NOT NULL,
+>>>>>>> Stashed changes
         opcao_a TEXT NOT NULL,
         opcao_b TEXT NOT NULL,
         opcao_c TEXT NOT NULL,
         opcao_d TEXT NOT NULL,
         opcao_e TEXT NOT NULL,
+<<<<<<< Updated upstream
         FOREING KEY (id_opcoes) REFERENCES atividades(id)
     );
     CREATE TABLE IF NOT EXISTS alunos(
@@ -18,6 +24,13 @@ cursor.execute('''
     )
     ''')
 conexao.commit()
+=======
+        FOREING KEY (id_atividade) REFERENCES atividades(id)
+    );
+    ''')
+conexao.commit()
+conexao.close()
+>>>>>>> Stashed changes
 # atividades
 def multipla_escolha(): #OK
     id_curso=int(input("Digite o identificador (ID) do curso: "))
@@ -28,6 +41,7 @@ def multipla_escolha(): #OK
     opcao_c=input("Digite a opção 'C': ")
     opcao_d=input("Digite a opção 'D': ")
     opcao_e=input("Digite a opção 'E': ")
+<<<<<<< Updated upstream
     resposta_correta=input("Digite a resposta correta: ")
     dica=input("Digite a dica: ")
     pontuacao=1
@@ -35,6 +49,16 @@ def multipla_escolha(): #OK
     cursor.execute('''INSERT INTO opcoes(id_opcoes, opcao_a, opcao_b, opcao_c, opcao_d, opcao_e) VALUES (?, ?, ?, ?, ?, ?)''', (numero_atividade, opcao_a, opcao_b, opcao_c, opcao_d, opcao_e))
     print("Atividade de Múltipla Escolha adicionada com sucesso!")
     conexao.commit()
+=======
+    respostas=input("Digite a resposta correta: ")
+    dica=input("Digite a dica: ")
+    pontuacao=1
+    cursor.execute('''INSERT INTO atividades(cursos_id, perguntas, respostas, dica, pontuacoes) VALUES (?, ?, ?, ?, ?)''', (id_curso, questão, respostas, dica, pontuacao))
+    cursor.execute('''INSERT INTO opcoes(id_atividade, opcao_a, opcao_b, opcao_c, opcao_d, opcao_e) VALUES (?, ?, ?, ?, ?, ?)''', (numero_atividade, opcao_a, opcao_b, opcao_c, opcao_d, opcao_e))
+    print("Atividade de Múltipla Escolha adicionada com sucesso!")
+    conexao.commit()
+    conexao.close()
+>>>>>>> Stashed changes
 def verdadeiro_falso(): #OK
     id_curso=int(input("Digite o identificador (ID) do curso: "))
     numero_atividade=int("Digite o número da atividade: ")
@@ -44,6 +68,7 @@ def verdadeiro_falso(): #OK
     alternativa_c=input("Digite a opção 'C': ")
     alternativa_d=input("Digite a opção 'D': ")
     alternativa_e=input("Digite a opção 'E': ")
+<<<<<<< Updated upstream
     resposta_correta=input("Digite a resposta correta (ex: A(V), B(F)): ")
     dica=input("Digite a dica: ")
     pontuacao=1
@@ -51,6 +76,16 @@ def verdadeiro_falso(): #OK
     cursor.execute('''INSERT INTO opcoes(id_opcoes, opcao_a, opcao_b, opcao_c, opcao_d, opcao_e) VALUES (?, ?, ?, ?, ?, ?)''', (numero_atividade, alternativa_a, alternativa_b, alternativa_c, alternativa_d, alternativa_e))
     print("Atividade de Verdadeiro e Falso adicionada com sucesso!")
     conexao.commit()
+=======
+    respostas=input("Digite a resposta correta (ex: A(V), B(F)): ")
+    dica=input("Digite a dica: ")
+    pontuacao=1
+    cursor.execute('''INSERT INTO atividades(cursos_id, perguntas, respostas, dica, pontuacoes) VALUES (?, ?, ?, ?, ?)''', (id_curso, questão, respostas, dica, pontuacao))
+    cursor.execute('''INSERT INTO opcoes(id_atividade, opcao_a, opcao_b, opcao_c, opcao_d, opcao_e) VALUES (?, ?, ?, ?, ?, ?)''', (numero_atividade, alternativa_a, alternativa_b, alternativa_c, alternativa_d, alternativa_e))
+    print("Atividade de Verdadeiro e Falso adicionada com sucesso!")
+    conexao.commit()
+    conexao.close()
+>>>>>>> Stashed changes
 def preencher_lacunas(): #OK
     id_curso=int(input("Digite o identificador (ID) do curso: "))
     numero_atividade=int("Digite o número da atividade: ")
@@ -60,6 +95,7 @@ def preencher_lacunas(): #OK
     opcao_c=input("Digite a opção 'C': ")
     opcao_d=input("Digite a opção 'D': ")
     opcao_e=input("Digite a opção 'E': ")
+<<<<<<< Updated upstream
     resposta_correta=input("Digite a resposta correta: ")
     dica=input("Digite a dica: ")
     pontuacao=1
@@ -67,6 +103,16 @@ def preencher_lacunas(): #OK
     cursor.execute('''INSERT INTO opcoes(id_opcoes, opcao_a, opcao_b, opcao_c, opcao_d, opcao_e) VALUES (?, ?, ?, ?, ?, ?)''', (numero_atividade, opcao_a, opcao_b, opcao_c, opcao_d, opcao_e))
     print("Atividade de Preencher Lacunas adicionada com sucesso!")
     conexao.commit()
+=======
+    respostas=input("Digite a resposta correta: ")
+    dica=input("Digite a dica: ")
+    pontuacao=1
+    cursor.execute('''INSERT INTO atividades(cursos_id, perguntas, respostas, dica, pontuacoes) VALUES (?, ?, ?, ?, ?)''', (id_curso, questão, respostas, dica, pontuacao))
+    cursor.execute('''INSERT INTO opcoes(id_atividade, opcao_a, opcao_b, opcao_c, opcao_d, opcao_e) VALUES (?, ?, ?, ?, ?, ?)''', (numero_atividade, opcao_a, opcao_b, opcao_c, opcao_d, opcao_e))
+    print("Atividade de Preencher Lacunas adicionada com sucesso!")
+    conexao.commit()
+    conexao.close()
+>>>>>>> Stashed changes
 def ordenar_etapas(): # OK
     id_curso=int(input("Digite o identificador (ID) do curso: "))
     numero_atividade=int("Digite o número da atividade: ")
@@ -76,6 +122,7 @@ def ordenar_etapas(): # OK
     etapa_c=input("Digite a etapa: ")
     etapa_d=input("Digite a etapa: ")
     etapa_e=input("Digite a etapa: ")
+<<<<<<< Updated upstream
     resposta_correta=input("Digite a resposta correta (ex: A(1), B(2)): ")
     dica=input("Digite a dica: ")
     pontuacao=1
@@ -92,5 +139,25 @@ def sequencia_logica(): # COLOCAR A TABELA 'opcoes'
     cursor.execute('''INSERT INTO atividades(cursor_id, perguntas, resposta_correta, dica, pontuacoes) VALUES (?, ?, ?, ?, ?)''', (id_curso, questão, resposta_correta, dica, pontuacao))
     print("Atividade de Sequência Lógica adicionada com sucesso!")
     conexao.commit()
+=======
+    respostas=input("Digite a resposta correta (ex: A(1), B(2)): ")
+    dica=input("Digite a dica: ")
+    pontuacao=1
+    cursor.execute('''INSERT INTO atividades(cursos_id, perguntas, respostas, dica, pontuacoes) VALUES (?, ?, ?, ?, ?)''', (id_curso, questão, respostas, dica, pontuacao))
+    cursor.execute('''INSERT INTO opcoes(id_atividade, opcao_a, opcao_b, opcao_c, opcao_d, opcao_e) VALUES (?, ?, ?, ?, ?, ?)''', (numero_atividade, etapa_a, etapa_b, etapa_c, etapa_d, etapa_e))
+    print("Atividade de Ordenar Etapas adicionada com sucesso")
+    conexao.commit()
+    conexao.close()
+def sequencia_logica(): # COLOCAR A TABELA 'opcoes'
+    id_curso=int(input("Digite o identificador (ID) do curso: "))
+    questão=input("Digite o enunciado da atividade: ")
+    respostas=input("Digite a resposta correta: ")
+    dica=input("Digite a dica: ")
+    pontuacao=1
+    cursor.execute('''INSERT INTO atividades(cursos_id, perguntas, respostas, dica, pontuacoes) VALUES (?, ?, ?, ?, ?)''', (id_curso, questão, respostas, dica, pontuacao))
+    print("Atividade de Sequência Lógica adicionada com sucesso!")
+    conexao.commit()
+    conexao.close()
+>>>>>>> Stashed changes
 def estrela(): # tabela de usuário (tipo aluno) necessária para receber
     cursor.execute('''INSERT INTO aluno(pontuacao) VALUES (?)''', (1))
